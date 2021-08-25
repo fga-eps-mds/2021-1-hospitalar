6268fe655917c66816556ed9762a135c61327cf2
Ricardo Loureiro <ricardoloureiro75@gmail.com>
2021-08-25 08:31:43 -0300

    #N/A - Ajustes na Branch

--- frontend/src/components/GlobalComponents/Header/styles.ts
+++
@@ -1,7 +0,0 @@
-import { makeStyles } from '@material-ui/core'
-
-export const useStyles = makeStyles((theme) => ({
-  typography: {
-    flex: 1,
-  },
-}))
--- frontend/src/theme/index.ts
+++
@@ -1,9 +0,0 @@
-import { createTheme } from '@material-ui/core/styles'
-
-/**
- * Tema usado no aplicativo.
- * Foi ativado na página index.tsx
- *
- * @see https://material-ui.com/customization/theming/#api
- */
-export const theme = createTheme({})
