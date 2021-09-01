efad23a23583a875e42ea4cd635ddcf8da009c8f
Eduardo Rodrigues de Farias <eduardo.rfarias@outlook.com>
2021-09-01 14:47:58 -0300

    #59 - Melhoria na funcionalidade de edição
    Foi modificado a funcionalidade de edição;
    Comentários importantes;
    Um arquivo para auxiliar na instalação de dependências do python.

--- backend/models/avaliacao/models.py
+++ backend/models/avaliacao/models.py
@@ -5,27 +5,32 @@ from django.db import models
 
 class Avaliacao(models.Model):
 
-    hospital = models.TextField(null=False, blank=False)
-    responsavel = models.TextField(null=False, blank=False)
+    hospital = models.TextField(blank=True)
+    responsavel = models.TextField(blank=True)
 
     def _str_(self):
         return self.hospital
 
     @property
     def analise(self):
+        '''
+            Esse método é nada mais que um dos atributos dessa classe,
+            ele representa uma lista de objetos Checklist.
+        '''
+
         return self.checklist_set.all()
 
 
 class Checklist(models.Model):
 
     avaliacao = models.ForeignKey(Avaliacao, on_delete=models.CASCADE)
-    secao = models.CharField(max_length=1)
-    indice = models.TextField(null=False, blank=False)
-    qualificacao = models.TextField(null=False, blank=False)
-    avaliacao_item = models.TextField(null=False, blank=False)
-    rota = models.TextField(null=False, blank=False)
-    comentario = models.TextField(null=False, blank=False)
-    status = models.TextField(null=False, blank=False)
+    secao = models.CharField(max_length=1, blank=True)
+    indice = models.TextField(blank=True)
+    qualificacao = models.TextField(blank=True)
+    avaliacao_item = models.TextField(blank=True)
+    rota = models.TextField(blank=True)
+    comentario = models.TextField(blank=True)
+    status = models.TextField(blank=True)
 
     def __str__(self):
         return self.secao
--- backend/models/avaliacao/serializers.py
+++ backend/models/avaliacao/serializers.py
@@ -1,4 +1,3 @@
-from dataclasses import fields
 from rest_framework import serializers
 from .models import Avaliacao, Checklist
 
@@ -21,6 +20,24 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
         fields = ('id', 'analise', 'hospital', 'responsavel')
 
     def create(self, validated_data):
+        '''
+            Esse método será executado ao fazer uma requisição POST e deverá criar
+            uma nova avaliação de acordo com o JSON passado no corpo da requisição.
+            Esse método se faz necessário pois o ModelSerializer não é capaz de
+            criar essa funcionalidade automaticamente quando a classe tem alguma
+            relação com outra classe.
+
+            O campo analise desta classe é uma lista do objeto Checklist, ilustrando 
+            assim uma relação de composição.
+
+            Parâmetro: 
+                validated_data: Objeto que representa o JSON passado como corpo da
+                                requisição.
+
+            Retorno:
+                O objeto criado.
+        '''
+
         analise = validated_data.pop('analise')
         avaliacao = Avaliacao.objects.create(**validated_data)
         for linha in analise:
@@ -29,14 +46,41 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
         return avaliacao
 
     def update(self, instance, validated_data):
+        '''
+            Esse método será executado ao fazer uma requisição PUT e deverá atualizar
+            a avaliação cujo id seja igual ao id passado pelo parâmetro da requisição.
+
+            Parâmetros: 
+                instance: Objeto a ser editado.
+                validated_data: Objeto que representa o JSON passado como corpo da
+                                requisição.
+
+            Retorno:
+                O objeto já editado.
+
+            PS: Para editar uma avaliação, o corpo da requisição deve conter o campo
+                analise pois este será usado para identificar quais analises criar, 
+                editar ou destruir. As analises, passadas para o JSON, que tiverem ID 
+                serão editadas, as que não tiverem ID serão criadas e as que existirem 
+                no objeto mas não existirem na requisição serão apagadas.
+        '''
+
         analise = validated_data.pop('analise')
+
         instance.hospital = validated_data.get("hospital", instance.hospital)
         instance.responsavel = validated_data.get(
             "responsavel", instance.responsavel)
+
         instance.save()
+
         keep_analises = []
         for linha in analise:
-            if "id" in linha.keys():
+
+            if "id" not in linha.keys():
+                c = Checklist.objects.create(**linha, avaliacao=instance)
+                keep_analises.append(c.id)
+
+            else:
                 if Checklist.objects.filter(id=linha["id"]).exists():
                     c = Checklist.objects.get(id=linha["id"])
 
@@ -54,9 +98,6 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
                     keep_analises.append(c.id)
                 else:
                     continue
-            else:
-                c = Checklist.objects.create(**linha, avaliacao=instance)
-                keep_analises.append(c.id)
 
         for linha in instance.analise:
             if linha.id not in keep_analises:
