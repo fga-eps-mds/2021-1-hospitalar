2fe7a84c876a10415f15b02f6369b0a7946f38fa
Daniel Alves <danielribeiroalves0@gmail.com>
2021-09-12 00:24:50 -0300

    Adicionandocampos de texto e configurando css da página.

--- frontend/src/pages/GenerateReport/index.tsx
+++ frontend/src/pages/GenerateReport/index.tsx
@@ -4,9 +4,12 @@ import { Header } from '../../components/GlobalComponents/Header'
 import { Form } from '../../components/GlobalComponents/Forms/Form';
 import { TextField } from '../../components/GlobalComponents/Inputs/TextField';
 import { Button } from '../../components/GlobalComponents/Inputs/Button';
+import { Typography } from '@material-ui/core'
+import { useStyles } from './styles'
 
 export const GenerateReport: React.FC = (props) => {
 
+    const classes = useStyles()
     let hospitalName: string = "";
     let error: number = 0;
     const [code , setCode] = useState('');
@@ -49,8 +52,9 @@ export const GenerateReport: React.FC = (props) => {
                     <Header title='Home Page' />
                 </Grid>
             </Grid>
-            <Box mt="10%">
+            <Box className={classes.form}>
                 <Grid container direction="column" justifyContent="flex-start" alignItems="center">
+                <Box mb="30px"><Typography variant="h3" align="center">Gerar Relatório</Typography></Box>
                     <Form>
                         <Grid container direction='column' spacing={2} alignItems="center" id="grid">
                             <TextField name="Código" value={code} label="Código" onChange={handleCodeChange} id="standard-basic" variant="outlined"/>
@@ -58,6 +62,29 @@ export const GenerateReport: React.FC = (props) => {
                             <Button size="medium" onClick={handleSubmit}>Baixar (.pdf)</Button>
                         </Grid>
                     </Form>
+                </Grid>
+            </Box>
+            <Box mt="100px">
+                <Grid className={classes.textContainer} container direction='column' spacing={2} alignItems="center">
+                    <Box borderColor="primary.main" border={3} mb="30px" className={classes.title}><Typography variant="h4" align="center">Descrição de Relatório</Typography></Box>
+                    <Box className={classes.text}>
+                        <Grid>
+                            <Box mb="10px"><Typography variant="h5" align="center">Demonstração de Gráfico</Typography></Box>
+                            <Typography>Texto testando Demonstração de Gráfico Texto testando Demonstração de GráficoTexto testando Demonstração de GráficoTexto testando Demonstração de GráficoTexto testando Demonstração de GráficoTexto testando Demonstração de GráficoTexto testando Demonstração de GráficoTexto testando Demonstração de GráficoTexto testando Demonstração de Gráfico</Typography>
+                        </Grid>
+                    </Box>
+                    <Box mt="30px" className={classes.text}>
+                        <Grid>
+                            <Box mb="10px"><Typography variant="h5" align="center">Legendas</Typography></Box>
+                            <Typography>Texto testando Legendas Texto testando LegendasTexto testando LegendasTexto testando LegendasTexto testando LegendasTexto testando LegendasTexto testando LegendasTexto testando LegendasTexto testando LegendasTexto testando LegendasTexto testando LegendasTexto testando LegendasTexto testando LegendasTexto testando LegendasTexto testando Legendas</Typography>
+                        </Grid>
+                    </Box>
+                    <Box mt="30px" className={classes.text}>
+                        <Grid>
+                            <Box mb="10px"><Typography variant="h5" align="center">Referências</Typography></Box>
+                            <Typography>Texto testando Referências Texto testando ReferênciasTexto testando ReferênciasTexto testando ReferênciasTexto testando ReferênciasTexto testando ReferênciasTexto testando ReferênciasTexto testando ReferênciasTexto testando ReferênciasTexto testando ReferênciasTexto testando ReferênciasTexto testando Referências</Typography>
+                        </Grid>
+                    </Box>
                 </Grid>
             </Box>
         </Grid>
--- frontend/src/pages/GenerateReport/styles.ts
+++ frontend/src/pages/GenerateReport/styles.ts
@@ -1,17 +1,29 @@
 import { makeStyles } from '@material-ui/core'
 
 export const useStyles = makeStyles((theme) => ({
+
+    title: {
+        width: '80%',
+    },
+    text: {
+        width: '75%',
+    },
     form: {
-        '& .MuiFormControl-root': {
-            margin: theme.spacing(1), 
-        }
+        margin: 'auto',
+        marginTop: '40px',
+        width: '35%',        
+        backgroundColor: '#29991a',
+        height: '300px',
+        borderRadius: '10px',
+        paddingTop: '15px',
     },
-    button: {
-        margin: theme.spacing(1),
-        width: '50%',
-        backgroundColor: '#ade489',
-        '&:hover': {
-            backgroundColor: '#f7f7e8',
-        }
-    },
+    textContainer: {
+        margin: 'auto',
+        width: '80%',
+        backgroundColor: '#f7f7e8',
+        paddingTop: '25px',
+        paddingBottom: '50px',
+        borderRadius: '10px',
+        marginBottom: '40px',
+    }
 }))
