e421c1e85b00e49269cb3fc1421a919eded44ce2
Pedro-V8 <pedrophdv@outlook.com>
2021-09-17 22:26:39 -0300

    #107-Editando arquivo settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -46,11 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-<<<<<<< HEAD
     'models.User'
-=======
     'models.geracaoPdf',
->>>>>>> ae861d4a1054f9b4305a1ccd9742884c3bbab362
 ]
 
 MIDDLEWARE = [
