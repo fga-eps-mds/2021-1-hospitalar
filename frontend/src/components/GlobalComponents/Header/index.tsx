36f8388f3f42696b8cef102b727f7ac5089b2e1c
Ricardo Loureiro <ricardoloureiro75@gmail.com>
2021-09-30 16:49:41 -0300

    #184 - Novo Header

--- frontend/src/components/GlobalComponents/Header/index.tsx
+++ frontend/src/components/GlobalComponents/Header/index.tsx
@@ -2,7 +2,7 @@ import {
   AccountCircleRounded as AccIcon,
   MenuRounded as MenuIcon,
 } from '@material-ui/icons'
-import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, IconButton, Toolbar, Typography, Box, Button } from '@material-ui/core'
 
 import React from 'react'
 import { useStyles } from './styles'
@@ -15,16 +15,18 @@ export function Header({ title }: Props): React.ReactElement {
   const classes = useStyles()
 
   return (
-    <AppBar position='static'>
-      <Toolbar>
-        <IconButton edge='start' color='inherit'>
-          <MenuIcon />
-        </IconButton>
-        <Typography className={classes.typography}>{title}</Typography>
-        <IconButton edge='end' color='inherit'>
-          <AccIcon />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton edge='start' color='inherit' aria-label='menu'>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' component='div'>
+            News
+          </Typography>
+          <Button color='inherit'>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
