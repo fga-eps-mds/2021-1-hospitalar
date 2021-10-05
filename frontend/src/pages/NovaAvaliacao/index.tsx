3583ac947205832875ec39d80dd51654126adc9d
SwampTG <adriansoareslps@gmail.com>
2021-10-05 20:49:23 -0300

    . #184 - Finalização Front-end com inputs - Tela Nova Aval.
    Também adicionadas novas dependências para utilizar DatePicker
    
    Co-authored-by: Eduardo <Eduardo-RFarias@users.noreply.github.com>
    Co-authored-by: Gustave <gpersijn@users.noreply.github.com>
    Co-authored-by: Ricardo <castroricardo1@users.noreply.github.com

--- frontend/src/pages/NovaAvaliacao/index.tsx
+++ frontend/src/pages/NovaAvaliacao/index.tsx
@@ -1,15 +1,21 @@
+import { DatePicker, LocalizationProvider } from '@material-ui/pickers'
 import { FormGroup, Grid, IconButton, Typography } from '@material-ui/core'
+import React, { useState } from 'react'
 
 import Autocomplete from '@material-ui/lab/Autocomplete'
+import { Button } from '../../components/GlobalComponents/Inputs/Button'
+import DateFnsAdapter from '@material-ui/pickers/adapter/date-fns'
 import { Form } from '../../components/GlobalComponents/Forms/Form'
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline'
-import React from 'react'
+import Logo from '../../assets/logo-2021-v2.png'
 import { Template } from '../../components/GlobalComponents/Template'
 import TextField from '@material-ui/core/TextField'
 import { useStyles } from './styles'
 
 export function NovaAvaliacao(): React.ReactElement {
   const classes = useStyles()
+
+  const [value, setValue] = useState<Date | null>(new Date())
 
   const generateForm = () => {
     const users = [
@@ -24,30 +30,104 @@ export function NovaAvaliacao(): React.ReactElement {
           direction='row'
           spacing={2}
           justifyContent='space-between'
+          alignContent='center'
           className={classes.formContainer}
+          xs={12}
         >
-          <Grid item container direction='column' xs={1} md={4}>
+          <Grid item container direction='column' xs={12} md={6} lg={4}>
             <Typography className={classes.avaliadorTitle}> Avaliadores </Typography>
             <Autocomplete
               multiple
               id='tags-standard'
               options={users}
-              getOptionLabel={(option) => option.name}
-              renderInput={(params) => (
+              getOptionLabel={(option: any) => option.name}
+              renderInput={(params: any) => (
                 <TextField
+                  className={classes.inputText}
                   {...params}
                   variant='standard'
                   label='Escolha os Avaliadores'
-                  placeholder='Favorites'
+                  placeholder='Avaliadores'
                 />
               )}
             />
+            <Button
+              onClick={() => null}
+              color='secondary'
+              className={classes.generateBtn}
+            >
+              GERAR
+            </Button>
           </Grid>
-          <Grid item direction='column' xs={4}>
-            Test
+          <Grid item direction='column' xs={12} md={6} lg={4} alignItems='center'>
+            <Grid>
+              <Typography className={classes.avaliadorTitle}> Hospital </Typography>
+              <TextField
+                className={classes.inputText}
+                variant='standard'
+                label='Digite o Hospital'
+                placeholder='Nome'
+              />
+            </Grid>
+            <Grid>
+              <Typography className={classes.avaliadorTitle}> Sigla </Typography>
+              <TextField
+                className={classes.inputText}
+                variant='standard'
+                label='Digite a Sigla do Hospital'
+                placeholder='Sigla'
+              />
+            </Grid>
+            <Grid>
+              <Typography className={classes.avaliadorTitle}> Data </Typography>
+              <LocalizationProvider dateAdapter={DateFnsAdapter}>
+                <DatePicker
+                  label='Basic example'
+                  value={value}
+                  onChange={(newValue) => setValue(newValue)}
+                  renderInput={(params: any) => (
+                    <TextField
+                      className={classes.inputText}
+                      {...params}
+                      variant='standard'
+                      label='Data da Avaliação'
+                      placeholder='Data'
+                    />
+                  )}
+                />
+              </LocalizationProvider>
+            </Grid>
           </Grid>
-          <Grid item direction='column' xs={4}>
-            Teste
+          <Grid
+            item
+            direction='column'
+            xs={12}
+            md={6}
+            lg={4}
+            justifyContent='space-evenly'
+          >
+            <Grid>
+              <img alt='Logo FAMil' src={Logo} className={classes.imgLogo} />
+            </Grid>
+            <Grid>
+              <Typography className={classes.avaliadorTitle}> Código </Typography>
+              <TextField
+                className={classes.inputText}
+                variant='standard'
+                label='Código da Avaliação'
+                placeholder='Código'
+              />
+            </Grid>
+          </Grid>
+          <Grid item container direction='row' xs={12} justifyContent='center'>
+            <Button
+              onClick={() => null}
+              color='secondary'
+              className={classes.generateBtn2}
+              size='large'
+            >
+              GERAR
+            </Button>
           </Grid>
         </Grid>
       </FormGroup>
@@ -59,14 +139,22 @@ export function NovaAvaliacao(): React.ReactElement {
       <Grid
         container
         direction='column'
-        alignItems='center'
+        alignItems='flex-start'
         className={classes.container}
       >
-        <Grid item container direction='row' className={classes.titleContainer}>
-          <Typography variant='h5' className={classes.title}>
-            {' '}
-            Gerar Novo Relatório{' '}
-          </Typography>
+        <Grid
+          item
+          container
+          direction='row'
+          className={classes.titleContainer}
+          justifyContent='space-between'
+        >
+          <Grid item xs={6} md={8} lg={8} alignItems='center' direction='row'>
+            <Typography variant='h5' className={classes.title}>
+              {' '}
+              Gerar Novo Relatório{' '}
+            </Typography>
+          </Grid>
           <IconButton color='secondary' edge='start' className={classes.helpIcon}>
             <HelpOutlineIcon fontSize='large' />
           </IconButton>
--- frontend/src/pages/NovaAvaliacao/styles.ts
+++ frontend/src/pages/NovaAvaliacao/styles.ts
@@ -3,30 +3,66 @@ import { makeStyles } from '@material-ui/core'
 export const useStyles = makeStyles((theme) => ({
   container: {
     width: '100%',
+    padding: 0,
   },
   title: {
     textTransform: 'uppercase',
     textAlign: 'center',
+    marginLeft: '15vw',
     color: `${theme.palette.secondary.main}`,
     fontWeight: 800,
   },
   titleContainer: {
-    width: 'auto',
+    width: '100%',
   },
   helpIcon: {
     position: 'relative',
-    left: '57%',
-    bottom: '22%',
+    bottom: '2vh',
   },
   formContainer: {
-    width: '62vw',
+    width: '60vw',
   },
   avaliadorTitle: {
     textTransform: 'uppercase',
-    backgroundColor: `${theme.palette.primary.main}`,
+    backgroundColor: `${theme.palette.secondary.main}`,
     color: 'white',
     fontWeight: 800,
     textAlign: 'center',
     margin: 0,
+  },
+  inputText: {
+    fontSize: '5px',
+  },
+  imgLogo: {
+    height: 100,
+    width: 90,
+    margin: 'auto',
+    display: 'None',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+      marginBottom: '11.7vh',
+    },
+  },
+  generateBtn: {
+    textTransform: 'uppercase',
+    borderRadius: '15px',
+    fontWeight: 800,
+    [theme.breakpoints.up('md')]: {
+      position: 'relative',
+      top: '12vh',
+    },
+    [theme.breakpoints.down('md')]: {
+      display: 'None',
+    },
+  },
+  generateBtn2: {
+    textTransform: 'uppercase',
+    borderRadius: '15px',
+    fontWeight: 800,
+    display: 'None',
+    [theme.breakpoints.down('md')]: {
+      display: 'flex',
+      width: '40vw',
+    },
   },
 }))
