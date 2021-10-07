b513b0b8cbe69c00cd7942673502999db594adf3
Augusto Durães Camargo <augusto.duraes33@gmail.com>
2021-10-06 23:27:45 -0300

    #210-tela-pre-visualizar-relatório:estrutura react
    Adição estrutura do react
    
    Co-authored-by: Magno <magnluiz@gmail.com>
    Co-authored-by: Daniel <danielribeiroalves0@gmail.com>

---
+++ frontend/src/pages/PreVisualizarRelatorio/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useStyles } from './styles'
+
+export function PreVisualizarRelatorio(): React.ReactElement {
+  const classes = useStyles()
+
+  return (
+    <>
+      <div>teste</div>
+    </>
+  )
+}
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -7,6 +7,7 @@ import { Login } from './pages/Login'
 import { NovaAvaliacao } from './pages/NovaAvaliacao'
 import { PaginaAddItem } from './pages/PaginaAddItem'
 import { PaginaAvaliacao } from './pages/PaginaAvaliacao'
+import { PreVisualizarRelatorio } from './pages/PreVisualizarRelatorio'
 import React from 'react'
 import { Template } from './components/GlobalComponents/Template'
 
@@ -27,6 +28,7 @@ export function Routes(): React.ReactElement {
         <Route exact path='/NovaAvaliacao/Adicao' component={PaginaAddItem} />
         <Route exact path='/relatorio' component={GenerateReport} />
         <Route exact path='/NovaAvaliacao' component={NovaAvaliacao} />
+        <Route exact path='/PreVisualizar' component={PreVisualizarRelatorio} />
       </Switch>
     </BrowserRouter>
   )
