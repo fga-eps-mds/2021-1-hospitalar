eb0d354d8da09957a0bde633bd94bdbb41d5095d
Eduardo Gurgel <51385738+EduardoGurgel@users.noreply.github.com>
2021-10-10 18:54:00 -0300

    #217 Método-GET

--- backend/models/Avaliacao/views.py
+++ backend/models/Avaliacao/views.py
@@ -2,6 +2,14 @@ from rest_framework import viewsets
 from .models import Avaliacao
 from .serializers import AvaliacaoSerializer
 
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+
+
+from django.core.files.storage import default_storage
+
 # Create your views here.
 
 
@@ -9,3 +17,12 @@ class AvaliacaoView(viewsets.ModelViewSet):
 
     serializer_class = AvaliacaoSerializer
     queryset = Avaliacao.objects.all()
+
+@csrf_exempt
+def avaliacaoApi(request,id=0):
+    if request.method=='GET':
+        avaliacaos = Avaliacaos.objects.all()
+        avaliacaos = Avaliacaos.objects.all()
+        avaliacaos_serializer=AvaliacaoSerializer(avaliacaos,many=True)
+        return JsonResponse(avaliacaos_serializer.data,safe=False)
+
