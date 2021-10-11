0ecd1101912ce293fd95a443dce0694395c2d17b
SwampTG <adriansoareslps@gmail.com>
2021-10-11 15:25:04 -0300

    . #159 - Utilizando modelo atualizado de Avaliação da Main
    Co-authored-by: Augusto <augustocrmg@users.noreply.github.com>
    Co-authored-by: Magno <magnluiz@users.noreply.github.com>

--- backend/models/Avaliacao/views.py
+++ backend/models/Avaliacao/views.py
@@ -1,6 +1,11 @@
 from rest_framework import viewsets
 from .models import Avaliacao
 from .serializers import AvaliacaoSerializer
+from django.http import HttpResponse
+from rest_framework.decorators import action
+from rest_framework import viewsets
+from .relatorio.printing import MyPrint
+from io import BytesIO
 
 # Create your views here.
 
@@ -9,3 +14,27 @@ class AvaliacaoView(viewsets.ModelViewSet):
 
     serializer_class = AvaliacaoSerializer
     queryset = Avaliacao.objects.all()
+
+    @action(methods=['get'], detail=False)
+    def generatePDF(self, request):
+        # Create the HttpResponse object with the appropriate PDF headers.
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+
+        # buffer armazena os bytes para o PDF (PDF é arquivo compilável)
+        buffer = BytesIO()
+
+        # exemplo para a avaliação cadastrada com código fgvgrad
+        getAval2 = Avaliacao.objects.get(codigo="fgvgrad")
+
+        # Utilizando o construtor para o Relatório
+        # buffer, Formato e Código da Aval.
+        report = MyPrint(buffer, 'A4', getAval2.codigo)
+
+        # Função que retorna o PDF utilizando o valor do buffer
+        pdf = report.printReport()
+
+        # Armazenando o PDF na resposta do servidor
+        response.write(pdf)
+
+        return response
