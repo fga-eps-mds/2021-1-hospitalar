8896994852063f847de5bf6744cb6885cdbcc8e2
Alladin-51 <igortls20@gmail.com>
2021-10-18 23:26:35 -0300

    Centralizacao dos butoes

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import { Grid } from '@material-ui/core'
 import { Header } from '../../components/GlobalComponents/Header'
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import { useStyles } from './styles'
 
 /**
  * Um componente funcional React compatível com React Hooks.
@@ -13,12 +14,13 @@ import { useHistory } from 'react-router-dom'
  */
 export function Home(): React.ReactElement {
   const history = useHistory()
+  const classes = useStyles()
   /**
    * A página foi criada utilizando a ferramenta de layout responsivo do material-ui
    * @see https://material-ui.com/components/grid/
    */
   return (
-    <Grid container direction='column' spacing={2}>
+    <><Grid container direction='column' spacing={2}>
       <Grid item>
         <Header
           links={[
@@ -26,17 +28,20 @@ export function Home(): React.ReactElement {
             { texto: 'Nova Avaliação', link: '/NovaAvaliacao' },
             { texto: 'Avaliações', link: '/avaliacao' },
             { texto: 'Relatórios', link: '/relatorio' },
-          ]}
-        />
+          ]} />
       </Grid>
-      <Grid item>
-        <Button onClick={() => history.push('relatorio')}>Página gerar relatorio</Button>
-      </Grid>
-      <Grid item>
-        <Button onClick={() => history.push('NovaAvaliacao')}>
-          Página nova avaliação
-        </Button>
-      </Grid>
+
     </Grid>
+    <Grid container direction='column' className={classes.centralizar} spacing={1} >
+        <Grid item>
+          <Button onClick={() => history.push('relatorio')}>Página gerar relatorio</Button>
+        </Grid>
+        <Grid item>
+          <Button onClick={() => history.push('NovaAvaliacao')}>
+            Página nova avaliação
+          </Button>
+        </Grid>
+      </Grid></>
+
   )
 }
